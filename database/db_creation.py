f551e10a7dc3e402ea71e6be0ccde0dbef28314a
yu <notyuhang@gmail.com>
2023-04-13 13:43:19 -0400

    initial sql creation code

--- database/db_creation.py
+++ database/db_creation.py
@@ -1,5 +1,7 @@
+'''
+Script to initialize database tables
+'''
 import sqlite3
-# Change to sql alchemy
 
 sql_cmd = ""
 db_name = "./dura.sqlite"
@@ -9,7 +11,30 @@ conn = sqlite3.connect(db_name)
 cur = conn.cursor()
 
 # Make SQL command
-sql_cmd += "CREATE TABLE IF NOT EXISTS objects"
+sql_cmd += '''
+CREATE TABLE IF NOT EXISTS objects (
+    object_id INTEGER PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    link VARCHAR NOT NULL,
+    image_link VARCHAR NOT NULL,
+    location_label VARCHAR NOT NULL,
+    location_name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL,
+    date VARCHAR NOT NULL,
+    creation_site VARCHAR NOT NULL,
+    details VARCHAR,
+    accession VARCHAR,
+    photographer VARCHAR,
+    negative_number VARCHAR,
+    repository VARCHAR,
+    photo_condition VARCHAR,
+    details2 VARCHAR,
+    image_source VARCHAR,
+    image_type VARCHAR,
+    language VARCHAR,
+    error VARCHAR,
+);
+'''
 
 # Execute SQL command
 cur.execute(sql_cmd)
