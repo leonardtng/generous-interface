36f4844197da949b13d2fbd683aa1f5ce915519d
Leonard Tng <leonardtng@gmail.com>
2023-04-18 13:56:42 -0400

    chore: clean up

--- client/database/db_operations.py
+++
@@ -1,9 +0,0 @@
-'''
-Provide functions for inserting or querying database
-'''
-import sqlite3
-
-# Insert row into database table
-def db_insert(values):
-
-    return []
--- client/flask_server/runserver.py
+++
@@ -1,8 +0,0 @@
-from flaskapp import app
-
-def main()
-    try:
-        app.run(host="0.0.0.0", port=8000, debug=True)
-    except Exception as e:
-        print(e)
-        exit(1)
